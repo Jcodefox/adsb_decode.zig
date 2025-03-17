1467b4a37abe6ae1fbef3707a185be1013589efa
Jcodefox <36343336+Jcodefox@users.noreply.github.com>
2025-03-16 21:28:32 -0400

    Add adsb prefix to everything in main.zig [hopefully works now]

--- src/main.zig
+++ src/main.zig
@@ -4,9 +4,9 @@ const crc = @import("crc.zig");
 const adsb = @import("root.zig");
 const Allocator = std.mem.Allocator;
 
-pub fn display_plane(plane: Plane, timestamp: i64, output: anytype, owner: [64]u8) !void {
+pub fn display_plane(plane: adsb.Plane, timestamp: i64, output: anytype, owner: [64]u8) !void {
     try output.print("{x:06}: {s}: ", .{ plane.icao, plane.callsign });
-    const coords: Coordinates = get_plane_coordinates(plane);
+    const coords: adsb.Coordinates = adsb.get_plane_coordinates(plane);
     // zig fmt: off
     try output.print(
         "Lat: {d:13.8}, Lon: {d:13.8}, Alt: {d:5} | Last heard {d:3}s ago ",
@@ -21,42 +21,42 @@ pub fn display_plane(plane: Plane, timestamp: i64, output: anytype, owner: [64]u
     // zig fmt: on
 }
 
-pub fn print_message_details(frame: Frame, plane: Plane, output: anytype) !void {
+pub fn print_message_details(frame: adsb.Frame, plane: adsb.Plane, output: anytype) !void {
     try output.print("{x:06}: {s}: ", .{ plane.icao, plane.callsign });
-    const message: UnknownMessage = @bitCast(frame.message);
+    const message: adsb.UnknownMessage = @bitCast(frame.message);
     switch (message.tc) {
-        MessageType.IDENT_FIRST...MessageType.IDENT_LAST => {
+        adsb.MessageType.IDENT_FIRST...adsb.MessageType.IDENT_LAST => {
             try output.print("Ident: {s} | ", .{plane.callsign});
             try output.print("{}\n", .{plane.wvc});
         },
-        MessageType.SURFACE_POS_FIRST...MessageType.SURFACE_POS_LAST => {
+        adsb.MessageType.SURFACE_POS_FIRST...adsb.MessageType.SURFACE_POS_LAST => {
             try output.print("Surface Position\n", .{});
         },
-        MessageType.AIR_POS_FIRST...MessageType.AIR_POS_LAST => {
+        adsb.MessageType.AIR_POS_FIRST...adsb.MessageType.AIR_POS_LAST => {
             // zig fmt: off
-            const coords: Coordinates = get_plane_coordinates(plane);
+            const coords: adsb.Coordinates = adsb.get_plane_coordinates(plane);
             try output.print(
                 "AirPos Baro: {d:.8}, {d:.8} | {d:5}\n",
                 .{ coords.lat, coords.lon - 0.0, plane.alt }
                 );
             // zig fmt: on
         },
-        MessageType.AIRBORNE_VELOCITIES => {
+        adsb.MessageType.AIRBORNE_VELOCITIES => {
             try output.print("Airborne Velocities\n", .{});
         },
-        MessageType.AIR_POS_GNSS_FIRST...MessageType.AIR_POS_GNSS_LAST => {
+        adsb.MessageType.AIR_POS_GNSS_FIRST...adsb.MessageType.AIR_POS_GNSS_LAST => {
             try output.print("Airborne Position w/ GNSS\n", .{});
         },
-        MessageType.RESERVED_FIRST...MessageType.RESERVED_LAST => {
+        adsb.MessageType.RESERVED_FIRST...adsb.MessageType.RESERVED_LAST => {
             try output.print("RESERVED\n", .{});
         },
-        MessageType.AIRCRAFT_STATUS => {
+        adsb.MessageType.AIRCRAFT_STATUS => {
             try output.print("Aircraft Status\n", .{});
         },
-        MessageType.TARGET_STATE_AND_STATUS_INFO => {
+        adsb.MessageType.TARGET_STATE_AND_STATUS_INFO => {
             try output.print("Target state and status info\n", .{});
         },
-        MessageType.AIRCRAFT_OPERATION_STATUS => {
+        adsb.MessageType.AIRCRAFT_OPERATION_STATUS => {
             try output.print("Aircraft operation status\n", .{});
         },
         else => {
@@ -65,7 +65,7 @@ pub fn print_message_details(frame: Frame, plane: Plane, output: anytype) !void 
     }
 }
 
-pub fn display_all_planes(planes_table: std.AutoHashMap(u24, Plane), faa_database_table: std.AutoHashMap(u24, faa.FAAPlane), output: anytype) !void {
+pub fn display_all_planes(planes_table: std.AutoHashMap(u24, adsb.Plane), faa_database_table: std.AutoHashMap(u24, faa.FAAPlane), output: anytype) !void {
     var plane_iter = planes_table.valueIterator();
     try output.print("=============\n", .{});
     while (plane_iter.next()) |p| {
@@ -110,7 +110,7 @@ pub fn main() !void {
     try stdout.print("Waiting for ADSB frames.\n", .{});
     try bw.flush();
 
-    var planes_table = std.AutoHashMap(u24, Plane).init(gpa);
+    var planes_table = std.AutoHashMap(u24, adsb.Plane).init(gpa);
     defer planes_table.deinit();
 
     while (true) {
@@ -131,8 +131,8 @@ pub fn main() !void {
             }
         }
 
-        const frame: Frame = @bitCast(frame_raw);
-        const plane: Plane = convert_frame_to_plane(frame, planes_table);
+        const frame: adsb.Frame = @bitCast(frame_raw);
+        const plane: adsb.Plane = adsb.convert_frame_to_plane(frame, planes_table);
         if (verbose) {
             try print_message_details(frame, plane, stdout);
         }
