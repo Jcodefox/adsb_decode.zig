fb2e9bc6a084578724fba46ca8f1f7a9369d6fc1
Jcodefox <jcodefox@gmail.com>
2025-03-16 22:16:49 -0400

    Write some comments in main.zig

--- src/main.zig
+++ src/main.zig
@@ -16,6 +16,7 @@ pub fn main() !void {
     var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{}){};
     const gpa = general_purpose_allocator.allocator();
 
+    // Do all the loading bits
     try stdout.print("Starting adsb.zig...\n", .{});
     try bw.flush();
 
@@ -37,13 +38,16 @@ pub fn main() !void {
     var planes_table = std.AutoHashMap(u24, adsb.Plane).init(gpa);
     defer planes_table.deinit();
 
+    // Main loop
     while (true) {
+        // Load hexadecimal input from stdin
         var input: [32]u8 = std.mem.zeroes([32]u8);
         _ = stdin.readUntilDelimiter(&input, '\n') catch {
             break;
         };
         const parsed_input: []u8 = input[1..29];
 
+        // Error check and correct
         var frame_raw: u112 = try std.fmt.parseInt(u112, parsed_input, 16);
         const frame_crc: u112 = crc.crc24(frame_raw);
         if (frame_crc != 0) {
@@ -55,12 +59,14 @@ pub fn main() !void {
             }
         }
 
+        // Handle frame
         const frame: adsb.Frame = @bitCast(frame_raw);
         const plane: adsb.Plane = adsb.convert_frame_to_plane(frame, planes_table);
         if (verbose) {
             try adsb_debug.print_message_details(frame, plane, stdout);
         }
 
+        // Update table and display
         try planes_table.put(frame.icao, plane);
         try adsb_debug.display_all_planes(planes_table, faa_database_table, stdout);
         try bw.flush();
